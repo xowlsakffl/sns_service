22a39741bac7a59aaff653a2e03050756cb643fc
msan <50791439+xowlsakffl@users.noreply.github.com>
2025-03-06 05:15:05 +0900

    프로젝트 생성

---
+++ src/main/java/dev/be/snsservice/SnsServiceApplication.java
@@ -0,0 +1,14 @@
+package dev.be.snsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SnsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsServiceApplication.class, args);
+    }
+
+}
